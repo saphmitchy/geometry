54ebab91417665b0aa015cd2b9713edbc7abbf66
saphmitchy <nakamichikohei135@g.ecc.u-tokyo.ac.jp>
2021-09-27 04:07:38 +0900

    add config

---
+++ src/config.hpp
@@ -0,0 +1,34 @@
+#ifndef GEOMETRY_CONFIG_HPP_
+#define GEOMETRY_CONFIG_HPP_
+
+#include <cmath>
+
+namespace sapphre15 {
+
+namespace geometry {
+
+// 座標を扱う型
+using Real = double;
+
+// 比較に用いる誤差定数
+const Real EPS = 1e-10;
+// 円周率
+const Real PI = std::acos(Real(-1.0));
+
+// 符号関数
+int sgn(Real a) {
+    return (a < -EPS) ? -1 : (a > EPS) ? 1 : 0;
+}
+// 値の比較
+bool eq(Real a, Real b) {
+    if (std::abs(a) > 1.0)
+        return std::abs((a - b) / a) < EPS;
+    else
+        return std::abs(a - b) < EPS;
+}
+
+} // namespace geometry
+
+} // namespace sapphre15
+
+#endif // GEOMETRY_CONFIG_HPP_
---
+++ test/config_test.cpp
@@ -0,0 +1,33 @@
+#include <config.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace sapphre15::geometry;
+
+TEST(sgn_test, sgn_test) {
+    EXPECT_EQ(sgn(5.5), 1);
+    EXPECT_EQ(sgn(0.00003), 1);
+    EXPECT_EQ(sgn(0.00000000001), 0);
+    EXPECT_EQ(sgn(0), 0);
+    EXPECT_EQ(sgn(-3), -1);
+    EXPECT_EQ(sgn(-0.00001), -1);
+}
+
+TEST(eq_test, eq_case) {
+    EXPECT_TRUE(eq(100, 100));
+    EXPECT_TRUE(eq(0, 0));
+    EXPECT_TRUE(eq(-1, -1));
+    EXPECT_TRUE(eq(-1, -1.000000000002));
+    EXPECT_TRUE(eq(1, 1.00000000009));
+    EXPECT_TRUE(eq(100000000010.0, 100000000008.0));
+}
+
+TEST(eq_test, neq_case) {
+    EXPECT_FALSE(eq(100, 99));
+    EXPECT_FALSE(eq(0, 0.1));
+    EXPECT_FALSE(eq(-1.0, 1.0));
+    EXPECT_FALSE(eq(-1.0, 0.1));
+    EXPECT_FALSE(eq(-1, -1.000000002));
+    EXPECT_FALSE(eq(0.0000001, 0.0));
+    EXPECT_FALSE(eq(10000000.0, 10000001.0));
+}
