e3fb068df92516a85fe96be72ca62f2a8aed85c6
saphmitchy <nakamichikohei135@g.ecc.u-tokyo.ac.jp>
2022-02-24 13:32:02 +0900

    add circle

--- src/circle.hpp
+++ src/circle.hpp
@@ -1,6 +1,8 @@
 #ifndef GEOMETRY_CIRCLE_HPP_
 #define GEOMETRY_CIRCLE_HPP_
 
+#include <cassert>
+#include <cmath>
 
 #include "config.hpp"
 #include "point.hpp"
@@ -11,24 +13,50 @@ namespace geometry {
 
 class Circle {
     public:
-    Circle();
+    Circle() : _center(), _radius(1.0) {}
     // 3点 c を中心として半径 r の円
-    Circle(const Point &c, const Real &r);
+    Circle(const Point &c, const Real &r)
+    : _center(c), _radius(r) {
+        assert(r >= 0);
+    }
     // 3点 c を中心として点 a を通る円
-    Circle(const Point &c, const Point &a);
+    Circle(const Point &c, const Point &a)
+    : _center(c), _radius(abs(c - a)) {}
     // 3点 a, b, c を通る円
-    Circle(const Point &a, const Point &b, const Point &c);
+    Circle(const Point &a, Point b, Point c) {
+        b -= a;
+        c -= a;
+        _center = Point(norm(b) * c.y() - norm(c) * b.y(),
+                        norm(c) * b.x() - norm(b) * c.x())
+                  / cross(b, c) / 2.0;
+        _center += a;
+        _radius = distance(_center, a);
+    }
 
     // 円の中心
-    Point center();
+    Point center() const {
+        return _center;
+    }
     // 円の半径
-    Real radius();
-    // 点pは円の内部か
-    bool inside(const Point &p);
+    Real radius() const {
+        return _radius;
+    }
+    // 点pは円の内部か(境界を含む)
+    bool inside(const Point &p) const {
+        return le(distance(_center, p), _radius);
+    }
     // 点pは円の外部か
-    bool outside(const Point &p);
+    bool outside(const Point &p) const {
+        return !le(distance(_center, p), _radius);
+    }
     // 点pは円周上か
-    bool on_object(const Point &p);
+    bool on_object(const Point &p) const {
+        return eq(_radius, distance(_center, p));
+    }
+
+    public:
+    Point _center;
+    Real _radius;
 };
 
 } // namespace geometry
---
+++ test/circle_test.cpp
@@ -0,0 +1,69 @@
+#include <circle.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace sapphre15::geometry;
+
+TEST(circleTest, onObjectTest) {
+    Circle c;
+    EXPECT_TRUE(c.on_object(Point(-1.0, 0.0)));
+    EXPECT_TRUE(c.on_object(Point(0.6, -0.8)));
+    EXPECT_FALSE(c.on_object(Point(0.9, 0.4)));
+}
+
+TEST(circleTest, paramConstructorTest) {
+    Circle c1(Point(0.5, -3.0), 4.0),
+           c2(Point(0.0, 0.0), 1.0);
+    EXPECT_TRUE(c1.on_object(Point(4.5, -3.0)));
+    EXPECT_TRUE(c1.on_object(Point(0.5, 1.0)));
+    EXPECT_FALSE(c1.on_object(Point(2.9, -0.8)));
+    EXPECT_TRUE(c2.on_object(Point(-1.0, 0.0)));
+    EXPECT_TRUE(c2.on_object(Point(0.6, -0.8)));
+    EXPECT_FALSE(c2.on_object(Point(0.9, 0.4)));
+}
+
+TEST(circleTest, point2ConstructorTest) {
+    Circle c(Point(3.0, -5.0), Point(3.7, -4.4));
+    EXPECT_TRUE(c.on_object(Point(2.1, -5.2)));
+    EXPECT_TRUE(c.on_object(Point(2.3, -4.4)));
+    EXPECT_FALSE(c.on_object(Point(2.1, -5.1)));
+}
+
+TEST(circleTest, centerTest) {
+    Circle c1(Point(-2.0, 5.0), 4.0);
+    Circle c2(Point(3.0, -6.0), Point(100.0, 3.0));
+    EXPECT_TRUE(c1.center() == Point(-2.0, 5.0));
+    EXPECT_TRUE(c2.center() == Point(3.0, -6.0));
+}
+
+TEST(circleTest, radiusTest) {
+    Circle c1(Point(-2.0, 5.0), 4.0);
+    Circle c2(Point(3.0, -6.0), Point(100.0, 3.0));
+    EXPECT_DOUBLE_EQ(c1.radius(), 4.0);
+    EXPECT_DOUBLE_EQ(c2.radius(), sqrt(9490));
+}
+
+TEST(circleTest, insideTest) {
+    Circle c(Point(-2.0, 5.0), 4.0);
+    EXPECT_TRUE(c.inside(Point(0.5, 2.5)));
+    EXPECT_TRUE(c.inside(Point(-5.0, 7.6)));
+    EXPECT_TRUE(c.inside(Point(2.0, 5.0)));
+    EXPECT_FALSE(c.inside(Point(0.0, .58)));
+}
+
+TEST(circleTest, outsideTest) {
+    Circle c(Point(-2.0, 5.0), 4.0);
+    EXPECT_TRUE(c.outside(Point(0.0, .58)));
+    EXPECT_TRUE(c.outside(Point(1.0, 2.0)));
+    EXPECT_FALSE(c.outside(Point(-5.0, 7.6)));
+    EXPECT_FALSE(c.outside(Point(2.0, 5.0)));
+}
+
+TEST(circleTest, point3ConstructorTest) {
+    Circle c1(Point(4855.0, -29.0), Point(-2406.0, 4218.0), Point(-1095.0, -4729.0)),
+           c2(Point(-2.0, 1.0), Point(-2.0, -3.0), Point(0.0, -1.0));
+    EXPECT_TRUE(c1.center() == Point(0.0, 1.0));
+    EXPECT_DOUBLE_EQ(c1.radius(), sqrt(23571925.0));
+    EXPECT_TRUE(c2.center() == Point(-2.0, -1.0));
+    EXPECT_DOUBLE_EQ(c2.radius(), 2.0);
+}
