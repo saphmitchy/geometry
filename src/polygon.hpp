6c61a8aa7104d7a33b09f140977e4b6b6013c049
saphmitchy <nakamichikohei135@g.ecc.u-tokyo.ac.jp>
2022-02-28 01:49:12 +0900

    add polygon

--- src/polygon.hpp
+++ src/polygon.hpp
@@ -1,9 +1,14 @@
 #ifndef GEOMETRY_CIRCLE_HPP_
 #define GEOMETRY_CIRCLE_HPP_
 
+#include <algorithm>
+#include <iterator>
+#include <memory>
 
 #include "config.hpp"
 #include "point.hpp"
+#include "segment.hpp"
+#include "intersection.hpp"
 
 namespace sapphre15 {
 
@@ -11,23 +16,64 @@ namespace geometry {
 
 class Polygon {
     public:
-    Polygon();
     // Container に含まれている点を順に並べた多角形
     template<class Container>
-    Polygon(Container &&point_list);
+    Polygon(Container &&points_list)
+    : _num(points_list.size()) {
+        points.reset(new Point[_num]);
+        std::copy(begin(points_list),
+                  end(points_list),
+                  begin(points));
+    }
 
     // 点pは多角形の内部にあるか
-    bool inside(const Point &p);
+    bool inside(const Point &p) {
+        Real sum = 0;
+        for(int i = 1; i < _num; i++) {
+            sum += cross(_points[i-1] - p, _points[i] - p);
+        }
+        sum += cross(_points[_num - 1] - p, _points[0] - p);
+        return eq(sum, 0.0);
+    }
     // 点pは多角形の境界上にあるか
-    bool on_object(const Point &p);
+    bool on_object(const Point &p) {
+        for(size_t i = 1; i < _num; i++) {
+            if(ccw(_points[i-1], _points[i], p) == ON_SEGMENT) {
+                return true;
+            }
+        }
+        return ccw(_points[0], _points[_num-1], p) == ON_SEGMENT;
+    }
     // 点pは多角形の外部にあるか
-    bool outside(const Point &p);
+    bool outside(const Point &p) {
+        return !inside(p);
+    }
     // 多角形は単純か
-    bool is_simple();
+    bool is_simple() {
+        for(int i = 1; i < _num; i++) {
+            for(int j = 1; j < i; j++) {
+                if(intersection(Segment(_points[i-1], _points[i]),
+                                Segment(_points[j-1], _points[j]))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
     // 多角形は凸か
-    bool is_convex();
-    // 多角形をxだけ反時計回りに回転させる
-    void rotate(const Real &x);
+    bool is_convex() {
+        ClockWise init = ccw(_points[_num-1], _points[0], _points[1]);
+        for(int i = 2; i < _num; i++) {
+            if(init != ccw(_points[i-2], _points[i-1], _points[i])) {
+                return false;
+            }
+        }
+        return init == ccw(_points[_num-2], _points[_num-1], _points[0]);
+    }
+
+    private:
+    const size_t _num;
+    std::unique_ptr<Point[]> _points;
 };
 
 } // namespace geometry
