cd3898dd0f02b6714c1338efbfac6dab0e293d15
saphmitchy <nakamichikohei135@g.ecc.u-tokyo.ac.jp>
2022-03-30 15:18:38 +0900

    add remove comment mode

--- AOJ/expander.py
+++ AOJ/expander.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python3
 
 import argparse
-from cmath import exp
 from typing import List
 import re
 
-include_file = re.compile("(?<=#include\s\")(src/|)[a-z_]+?(|.hpp)(?=\")")
-include_guard = re.compile("GEOMETRY_")
-SOURCE_PATH = "../src/"
+include_file = re.compile('(?<=#include\s\")(src/|)[a-z_]+?(|.hpp)(?=\")')
+include_guard = re.compile('GEOMETRY_')
+SOURCE_PATH = '../src/'
+
+'''
+コメントアウトを削除する。
+すべて削除する場合はNone を返し、
+それ以外は削除後の文字列を返す
+'''
+def comment_remover(line : str) -> str:
+    if '/*' in line or \
+       '*/' in line or \
+        re.match('^//.*', line) or \
+        re.match('^ \* .*', line):
+        return None
+    else:
+        return  re.sub(r'(.*)//(.*)', r'\1', line)
 
 expand = set()
-
-def dfs(inputFile : str) -> List[str]:
+def dfs(inputFile : str, remove_comment : bool) -> List[str]:
     ret = []
     with open(inputFile) as f:
         lines = f.readlines()
 
     for line in lines:
-        if include_guard.search(line):
+        if include_guard.search(line) or line == "\n":
             continue
         if include_file.search(line):
             filePath = include_file.search(line).group(0)
@@ -26,18 +38,24 @@ def dfs(inputFile : str) -> List[str]:
             if filePath in expand:
                 continue
             expand.add(filePath)
-            ret = ret + dfs(SOURCE_PATH + filePath)
+            ret = ret + dfs(SOURCE_PATH + filePath, remove_comment)
         else:
-            ret.append(line)
+            if remove_comment:
+                removed = comment_remover(line)
+                if removed:
+                    ret.append(removed)
+            else:
+                ret.append(line)
     return ret
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="indlude expander")
-    parser.add_argument("input", nargs=1, type=str)
-    parser.add_argument("--output", nargs=1, default="output.cpp", type=str)
+    parser = argparse.ArgumentParser(description='indlude expander')
+    parser.add_argument('input', nargs=1, type=str)
+    parser.add_argument('--output', nargs=1, default='output.cpp', type=str)
+    parser.add_argument('--remove_comment', action='store_true')
     
     args = parser.parse_args()
-    lines = dfs(args.input[0])
+    lines = dfs(args.input[0], args.remove_comment)
 
     with open(args.output, 'w') as f:
         for i in lines:
