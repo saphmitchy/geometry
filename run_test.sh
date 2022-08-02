f596bd15a167aa1de3bb3bcf705422e355713b17
saphmitchy <nakamichi-kohei135@g.ecc.u-tokyo.ac.jp>
2022-08-02 16:32:05 +0900

    add coverage

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-cmake -S . -B build && cmake --build build && cd build && ctest
+
+# build
+cmake -S . -B build && cmake --build build
+
+if [[ $? -eq 0 ]]; then
+    # run test
+    cd build
+    ctest
+
+    # generate coverage info
+    cd test/CMakeFiles
+    lcov -d . -c -o  converage.info
+    lcov -r converage.info */googletest/* test/* */c++/* -o coverageFiltered.info
+    genhtml -o lcovHtml --num-spaces 4 -s --legend coverageFiltered.info
+
+    if [[ $? -eq 0 ]]; then
+        echo output file: `pwd`/lcovHtml/index.html
+    fi
+fi
