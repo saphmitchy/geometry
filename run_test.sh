6720789eb333b604c4ecdfabe65217682a4ad849
saphmitchy <nakamichi-kohei135@g.ecc.u-tokyo.ac.jp>
2022-12-19 13:58:49 +0900

    update

--- run_test.sh
+++ run_test.sh
@@ -8,13 +8,15 @@ if [[ $? -eq 0 ]]; then
     cd build
     ctest
 
-    # generate coverage info
-    cd test/CMakeFiles
-    lcov -d . -c -o  converage.info
-    lcov -r converage.info */googletest/* test/* */c++/* -o coverageFiltered.info
-    genhtml -o lcovHtml --num-spaces 4 -s --legend coverageFiltered.info
-
     if [[ $? -eq 0 ]]; then
-        echo output file: `pwd`/lcovHtml/index.html
+        # generate coverage info
+        cd test/CMakeFiles
+        lcov -d . -c -o  converage.info
+        lcov -r converage.info */googletest/* test/* */c++/* -o coverageFiltered.info
+        genhtml -o lcovHtml --num-spaces 4 -s --legend coverageFiltered.info
+
+        if [[ $? -eq 0 ]]; then
+            echo output file: `pwd`/lcovHtml/index.html
+        fi
     fi
 fi
